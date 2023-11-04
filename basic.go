c2d32bf9ce4f258409aaee84e5839dae3d54c92e
toddymon <toddymonkung@gmail.com>
2023-11-04 12:59:31 +0700

    first basic command

---
+++ basic.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+var c, python, java bool
+
+func main() {
+	fmt.Println("============= Slide 1 ====================")
+	fmt.Println("This is my 1 golang, random number is ", rand.Intn(10))
+	fmt.Print("\n\n\n============= Slide 2 ====================\n")
+	var i int
+	fmt.Println(i, c, python, java)
+	fmt.Print("\n\n\n============= Slide 3 ====================\n")
+
+}
