63e3e67892273dfea26acdb741958653669fb40c
toddymon <toddymonkung@gmail.com>
2023-11-04 13:03:56 +0700

    convert type example

--- basic2.go
+++ basic2.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var i int
@@ -9,4 +12,9 @@ func main() {
 	var s string
 	var r rune
 	fmt.Printf("%v %v %v %v %q\n", i, f, b, r, s)
+
+	var x, y int = 3, 4
+	var f1 float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f1)
+	fmt.Println(x, y, z)
 }
